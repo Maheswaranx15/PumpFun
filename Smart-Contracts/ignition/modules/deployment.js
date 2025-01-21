0d0b9dab3b4d57f8c7cb3fcb99d9f1fba4d00f1b
Maheswaranx15 <maheshwaran66648@gmail.com>
2025-01-21 22:20:33 +0530

    smart contract configured

--- Smart-Contracts/ignition/modules/deployment.js
+++ Smart-Contracts/ignition/modules/deployment.js
@@ -1,15 +1,15 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 
-module.exports = buildModule("LockModule", (m) => {
+module.exports = buildModule("EventContractModule", (m) => {
   // const initialTokenOwner = m.getParameter("initialOwner", "0xE678e0faEe40ffCF078854bac2ED20fA8dA8C4B0");
   // const MockERC20 = m.contract("MockERC20",[initialTokenOwner]);
-  const _feeRecipient = m.getParameter("_feeRecipient", "0xa7e793bc4D5BEBE56739f89994316Ed9850E3aF2"); 
+  const _feeRecipient = m.getParameter("_feeRecipient", '0x16e9090A785c63E7Ed0C8a2D6d722e73bAac5E8B'); 
   const _platformFee = m.getParameter("_platformFee", 100);
   const PumpFun = m.contract("PumpFun",[_feeRecipient,_platformFee]);
 
   return { PumpFun };
 });
 
-//token -- 0xa7e793bc4D5BEBE56739f89994316Ed9850E3aF2
-//pumpFun -- 0xDDCA982C2f64626d0776c1487ccCAa874275E0df
+//token -- 0x16e9090A785c63E7Ed0C8a2D6d722e73bAac5E8B
+//pumpFun -- 0xAB0Ee22F38f09e9F6a3a121C5375f46Eb7a49A70
