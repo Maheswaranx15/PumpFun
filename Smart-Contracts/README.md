08bb7dbc9aae1a67af09fd64a0bd34cc41e20efe
Maheswaranx15 <maheshwaran66648@gmail.com>
2025-01-21 19:02:37 +0530

    contract deployed and tested

--- Smart-Contracts/README.md
+++ Smart-Contracts/README.md
@@ -11,3 +11,8 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat ignition deploy ./ignition/modules/Lock.js
 ```
+MockTokenERC20 Contract Deployed on Holesky testnet :
+https://holesky.etherscan.io/address/0x16e9090A785c63E7Ed0C8a2D6d722e73bAac5E8B#code
+
+EventContract Deployed on Holesky testnet :
+https://holesky.etherscan.io/address/0xAB0Ee22F38f09e9F6a3a121C5375f46Eb7a49A70#code
--- Smart-Contracts/hardhat.config.js
+++ Smart-Contracts/hardhat.config.js
@@ -10,15 +10,15 @@ module.exports = {
   solidity: "0.8.25",
 
   networks: {
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    holesky: {
+      url: `https://eth-holesky.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY]
     }
   },
 
   etherscan: {
     apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
+      holesky: ETHERSCAN_API_KEY,
     },
   },
 };
